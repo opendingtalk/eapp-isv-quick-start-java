73d76a78e0d332170ed54357b9b03e65f4cbafab
lifeng.zlf <lifeng.zlf@alibaba-inc.com>
2018-05-30 21:08:40 +0800

    add

--- src/main/java/com/config/Constant.java
+++ src/main/java/com/config/Constant.java
@@ -7,9 +7,9 @@ public class Constant {
     /**
      * 套件的SuiteKey，登录开发者后台，点击套件管理，进入套件详情可见
      */
-    public static final String SUITE_KEY="suitexnh4qtqzl91jjxzr";
+    public static final String SUITE_KEY="";
     /**
      * 套件的SuiteSecret，登录开发者后台，点击套件管理，进入套件详情可见
      */
-    public static final String SUITE_SECRET="5KC5HKsKVKJJkKEgIZ23D3pUgb5IiIoIQZWPTGSi4yvtH7qcD_abHrVUAcs9Icbk";
+    public static final String SUITE_SECRET="";
 }
