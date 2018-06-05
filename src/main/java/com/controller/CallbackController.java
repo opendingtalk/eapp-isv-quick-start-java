c03e070ad9c886d6f6a95dd966e8d6e3c4819fab
bruce.lxm <bruce.lxm@alibaba-inc.com>
2018-06-05 14:05:20 +0800

    回调事件注释

--- src/main/java/com/controller/CallbackController.java
+++ src/main/java/com/controller/CallbackController.java
@@ -74,8 +74,14 @@ public class CallbackController {
             } else if (EVENT_CHECK_UPADTE_SUITE_URL.equals(eventType)) {
                 bizLogger.info("验证更新回调URL有效性: " + plainText);
             } else if (EVENT_SUITE_TICKET.equals(eventType)) {
+                //suite_ticket用于用签名形式生成accessToken(访问钉钉服务端的凭证)，需要保存到应用的db。
+                //钉钉会定期向本callback url推送suite_ticket新值用以提升安全性。
+                //应用在获取到新的时值时，保存db成功后，返回给钉钉success加密串（如本demo的return）
                 bizLogger.info("应用suite_ticket数据推送: " + plainText);
             } else if (EVENT_TMP_AUTH_CODE.equals(eventType)) {
+                //本事件应用应该异步进行授权开通企业的初始化，目的是尽最大努力快速返回给钉钉服务端。用以提升企业管理员开通应用体验
+                //即使本接口没有收到数据或者收到事件后处理初始化失败都可以后续再用户试用应用时从前端获取到corpId并拉取授权企业信息，
+                // 进而初始化开通及企业。
                 bizLogger.info("企业授权开通应用事件: " + plainText);
             } else {
                 // 其他类型事件处理
