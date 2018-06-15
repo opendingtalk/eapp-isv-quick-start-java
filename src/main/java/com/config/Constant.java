96b05b3978d0f5df8e38883e53b58cca563714fc
bruce.lxm <bruce.lxm@alibaba-inc.com>
2018-06-16 00:57:57 +0800

    getAuthCode

--- src/main/java/com/config/Constant.java
+++ src/main/java/com/config/Constant.java
@@ -7,19 +7,19 @@ public class Constant {
     /**
      * 应用的SuiteKey，登录开发者后台，点击应用管理，进入应用详情可见
      */
-    public static final String SUITE_KEY="";
+    public static final String SUITE_KEY="suitemytjg3az5et44vcz";
     /**
      * 应用的SuiteSecret，登录开发者后台，点击应用管理，进入应用详情可见
      */
-    public static final String SUITE_SECRET="";
+    public static final String SUITE_SECRET="w6XsRg6mO8EqqY5vo9UfhE2HL3oq1p_4SMj2yuUFgeMqTjr3CW17hc48qsllAXSV";
 
     /**
      * 回调URL加解密用。应用的数据加密密钥，登录开发者后台，点击应用管理，进入应用详情可见
      */
-    public static final String ENCODING_AES_KEY = "";
+    public static final String ENCODING_AES_KEY = "17xfe8k2sitcymbwjd3jd5w8fss3tmr66vrlj6vb58x";
 
     /**
      * 回调URL签名用。应用的签名Token, 登录开发者后台，点击应用管理，进入应用详情可见
      */
-    public static final String TOKEN = "";
+    public static final String TOKEN = "jiemitoken";
 }
